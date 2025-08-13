dbfac7a793aa0607a8a61d9d68d026866614d902
Adrika Banerjee <adrikabanerje019@gmail.com>
2025-08-13 13:31:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -26,11 +26,26 @@ addTaskBtn.addEventListener("click", () => {
 
 function renderTask(task){
      const li =document.createElement("li");
-     li.setAttribute('data-id',tasks.id)
+     li.setAttribute('data-id',task.id)
+     if(task.completed) li.classList.add('completed');
      li.innerHTML = `
-     <span>${task.text}</span>
-     <button>delete</button>
-     `
+       <span>${task.text}</span>
+       <button>delete</button>
+     `;
+     li.addEventListener('click',(e) => {
+        if(e.target.tagName === 'BUTTON') return;
+        task.completed = !task.completed;
+        li.classList.toggle('completed',task.completed);
+        saveTasks();
+     })
+
+     li.querySelector('button').addEventListener('click', (e) => {
+        e.stopPropagation(); // Prevent the click from toggling completion
+        const taskId = li.getAttribute('data-id');
+        tasks = tasks.filter(t => t.id != taskId);
+        li.remove();
+        saveTasks();
+     });
      taskList.appendChild(li);
     }
 
